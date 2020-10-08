ae7d2b195bf7278250df388ff842125bd8c98f24
scrubjay55 <karayay12@gmail.com>
2020-10-08 17:34:24 +0300

    update.. stuff

--- setup.py
+++ setup.py
@@ -2,9 +2,13 @@ from setuptools import setup
 
 setup(
 name='Reddit_ChatBot_Python',
-version='1',
+version='1.1',
+project_urls={"Source": "https://github.com/scrubjay55/Reddit_ChatBot_Python.git"},
+url="https://github.com/scrubjay55/Reddit_ChatBot_Python.git",
 author='scrubjay55',
+license="APLv2",
 packages=['Reddit_ChatBot_Python', 'Reddit_ChatBot_Python.Utils', 'Reddit_ChatBot_Python.Utils.FrameModel'],
+scripts=["dump_access_token/dump_access_token.py"],
 description='a pretty basic websocket wrapper for reddit chatrooms by u/peroksizom',
 long_description=open('README.rst').read(),
 install_requires=[
