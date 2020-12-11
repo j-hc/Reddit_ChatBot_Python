d9bd992a0b9761dd6d88e70bcb5f6a9a1ffd3ba6
scrubjay55 <karayay12@gmail.com>
2020-12-11 19:42:27 +0300

    RedditAuthentication: exception handling

--- Reddit_ChatBot_Python/RedditAuthentication.py
+++ Reddit_ChatBot_Python/RedditAuthentication.py
@@ -23,7 +23,14 @@ class PasswordAuth:
             'passwd': self.reddit_password
         }
         response = requests.post('https://www.reddit.com/post/login', headers=headers, data=data, allow_redirects=False)
-        redditsession = response.cookies.get("reddit_session")
+        try:
+            redditsession = response.cookies["reddit_session"]
+        except KeyError:
+            raise Exception("Wrong username or password")
         chat_r = requests.get('https://www.reddit.com/chat/', headers=headers, cookies={"reddit_session": redditsession})
-        sendbird_scoped_token = re.search(b'"accessToken":"(.*?)"', chat_r.content).group(1).decode()
+        token_r = re.search(b'"accessToken":"(.*?)"', chat_r.content)
+        if token_r is None:
+            raise Exception("Can't get token because of an unknown reason")
+        else:
+            sendbird_scoped_token = token_r.group(1).decode()
         return sendbird_scoped_token
