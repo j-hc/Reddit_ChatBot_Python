a27951ffa70b4ab27edaa3c20c5a1f6fa17add05
scrubjay55 <karayay7@gmail.com>
2021-02-25 19:16:55 +0300

    version 1.2.2

--- setup.py
+++ setup.py
@@ -1,20 +1,21 @@
 from setuptools import setup
 
 setup(
-name='Reddit_ChatBot_Python',
-version='1.2.1',
-project_urls={"Source": "https://github.com/scrubjay55/Reddit_ChatBot_Python.git"},
-url="https://github.com/scrubjay55/Reddit_ChatBot_Python.git",
-author='scrubjay55',
-license="APLv2",
-packages=['Reddit_ChatBot_Python', 'Reddit_ChatBot_Python.Utils'],
-description='a pretty basic websocket wrapper for reddit chatrooms',
-long_description=open('README.rst').read(),
-python_requires='<=3.8.7',
-install_requires=[
-   "requests",
-   "websocket_client",
-   "wsaccel",
-   "numpy"
-],
+    name='Reddit_ChatBot_Python',
+    version='1.2.2',
+    project_urls={"Source": "https://github.com/scrubjay55/Reddit_ChatBot_Python.git"},
+    url="https://github.com/scrubjay55/Reddit_ChatBot_Python.git",
+    author='scrubjay55',
+    license="APLv2",
+    packages=['Reddit_ChatBot_Python', 'Reddit_ChatBot_Python.Utils'],
+    description='a pretty basic websocket wrapper for reddit chatrooms',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    python_requires='<=3.8.7',
+    install_requires=[
+        "requests",
+        "websocket_client",
+        "wsaccel",
+        "numpy"
+    ],
 )
