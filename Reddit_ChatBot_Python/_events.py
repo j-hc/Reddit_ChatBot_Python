8a74aa6ae947f35b9199e877a944702d749bd5aa
scrubjay55 <karayay12@gmail.com>
2021-12-27 00:30:11 +0300

    added event on_image

--- Reddit_ChatBot_Python/_events.py
+++ Reddit_ChatBot_Python/_events.py
@@ -27,6 +27,15 @@ class Events:
 
         return wrap(func)
 
+    def on_image(self, func: _hook = None, run_parralel=False):
+        def wrap(func):
+            return self.on_any(func, FrameType.MEDI, run_parralel)
+
+        if func is None:
+            return wrap
+
+        return wrap(func)
+
     def on_broadcast(self, func: _hook = None, run_parralel=False):
         def wrap(func):
             return self.on_any(func, FrameType.BRDM, run_parralel)
